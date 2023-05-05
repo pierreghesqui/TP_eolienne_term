c82051a25da855114b2e6f14cc463cf2fa3e5a5f
DESKTOP-C03PQIV\pierr <pierre.ghesquiere@lilo.org>
2023-05-05 12:40:42 +0200

    no message

--- fonctions.py
+++ fonctions.py
@@ -61,12 +61,13 @@ def affichage(x,y,vx,vy,ax,ay,t,v,filtrageSize = 5):
     
     resized = img[:,400:900]
     #cv2.resize(img, (int(nbC/2),int(nbL/2)), interpolation = cv2.INTER_AREA)
-    plt.figure(figsize=(16,10))
+    plt.figure(num = 1, figsize=(16,10))
     plt.imshow(resized)
     
     vmoy = np.mean(v[6:len(v)])
     plt.show()
-    plt.figure()
+    
+    plt.figure(num = 2)
     plt.plot(t[6:-1],v[6:len(v)],label='v')
     plt.plot(t[6:-1],np.ones(len(v)-6)*vmoy,label='vitesse moyenne = '+ str(int(vmoy)) + ' m/s')
     
